d16fa83af3590ccaf1cfeb3192bae53d4d48069d
NickZaranis <nikolaoszaranis@gmail.com>
2019-03-18 19:53:32 +0200

    v0.1

---
+++ src/notepad/Notepad.java
@@ -0,0 +1,19 @@
+package notepad;
+
+/**
+ *
+ * @author Stagonas
+ */
+
+
+public class Notepad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       NotepadGUI np = new NotepadGUI();
+       
+    }
+    
+}
